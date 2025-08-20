f9258d86984e2e0e426406a77f125b2310cde035
vishal payyawar <139535850+vishalseries7030@users.noreply.github.com>
2025-08-20 09:59:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function highlight() {
-    //Write your code here
-
-
+  const boldWords = document.querySelectorAll("strong");
+  boldWords.forEach(word => {
+    word.style.color = "rgb(0, 128, 0)"; // green
+  });
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+  const boldWords = document.querySelectorAll("strong");
+  boldWords.forEach(word => {
+    word.style.color = "rgb(0, 0, 0)"; // black
+  });
 }
